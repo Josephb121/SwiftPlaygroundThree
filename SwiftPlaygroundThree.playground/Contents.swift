fd9ce90aaef9ff37460c478863a40984b547caf3
CM Student <cmstudent@uhwo-acm-13.local>
2019-02-14 10:30:09 -1000

    Finished SwiftPlaygroundsThree

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// Part 2
+//declaration
+var fruitNames: [String] = ["Watermelon","Strawberry","Mango"]
+//assign fruitNames with 3 of your favorite fruits
+//Part 4
+print(fruitNames)
+print("I like to eat\(fruitNames)")
+//Part 6
+for name in fruitNames {
+    print("I like to eat" + name)
+}
+
+print(fruitNames[1])
+
+print(fruitNames[0])
